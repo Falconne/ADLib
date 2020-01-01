9c0c737d86db2575b5b9f44aec8c6b9ec3484675
Anuradha Dissanayake <anuradha.dissanayake@enghouse.com>
2020-01-02 00:18:50 +1300

    Restart helper

---
+++ AKBDLib/Util/WindowsHost.cs
@@ -0,0 +1,13 @@
+﻿using AKBDLib.Logging;
+
+namespace AKBDLib.Util
+{
+    public static class WindowsHost
+    {
+        public static void Restart(int delay)
+        {
+            GenLog.Info($"Restarting computer in {delay} seconds");
+            Shell.RunAndFailIfNotExitZeroMS($"shutdown /r /t {delay}");
+        }
+    }
+}
