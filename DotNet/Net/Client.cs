c39ae1fbf634a5cfa375f46b76d4ea929472776b
Anuradha Dissanayake <falconne@gmail.com>
2020-09-27 21:08:42 +1300

    Cancel retry-able operations

--- DotNet/Net/Client.cs
+++ DotNet/Net/Client.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ADLib.Net
@@ -19,25 +20,28 @@ namespace ADLib.Net
             }
         }
 
-        public static async Task<string> DownloadFileAsync(string url, string destination)
+        public static async Task<string> DownloadFileAsync(string url, string destination,
+            CancellationToken cancellationToken)
         {
             var destinationDirectory = Path.GetDirectoryName(destination);
             FileSystem.CreateDirectory(destinationDirectory);
 
             await Retry.OnExceptionAsync(
-                async () => { await DownloadFileOrFailAsync(url, destination); },
+                async () => { await DownloadFileOrFailAsync(url, destination, cancellationToken); },
 
-                $"Downloading {url} to {destination}");
+                $"Downloading {url} to {destination}",
+                cancellationToken);
 
             return destination;
         }
 
-        private static async Task DownloadFileOrFailAsync(string url, string destination)
+        private static async Task DownloadFileOrFailAsync(
+            string url, string destination, CancellationToken cancellationToken)
         {
             using (var client = new WebClient())
             {
                 await Task.Run(() =>
-                    client.DownloadFile(url, destination));
+                    client.DownloadFile(url, destination), cancellationToken);
             }
 
             if (!File.Exists(destination))
--- DotNet/Util/Retry.cs
+++ DotNet/Util/Retry.cs
@@ -1,4 +1,5 @@
-﻿using JetBrains.Annotations;
+﻿using ADLib.Logging;
+using JetBrains.Annotations;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,31 +9,37 @@ namespace ADLib.Util
     public static class Retry
     {
         public static async Task OnExceptionAsync(
-            [InstantHandle] Func<Task> action, string introMessage, int numRetries = 3, int delay = 3000)
+            [InstantHandle] Func<Task> action, string introMessage, CancellationToken cancellationToken, int numRetries = 3, int delay = 3000)
 
         {
             if (numRetries < 0)
                 numRetries = 0;
 
-            while (numRetries-- > 0)
+            while (numRetries-- > 0 && !cancellationToken.IsCancellationRequested)
             {
                 try
                 {
-                    Logging.GenLog.Info(introMessage);
+                    GenLog.Info(introMessage);
                     await action();
                     return;
                 }
                 catch (Exception e)
                 {
-                    Logging.GenLog.Warning("Caught exception during retry-able operation:");
-                    Logging.GenLog.Warning(e.Message);
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        GenLog.Warning("Cancelling retry-able operation");
+                        return;
+                    }
+
+                    GenLog.Warning("Caught exception during retry-able operation:");
+                    GenLog.Warning(e.Message);
                     if (numRetries == 0)
                     {
-                        Logging.GenLog.Error("No more retries left");
+                        GenLog.Error("No more retries left");
                         throw;
                     }
-                    Logging.GenLog.Info($"Retries remaining: {numRetries}");
-                    Thread.Sleep(delay);
+                    GenLog.Info($"Retries remaining: {numRetries}");
+                    await Task.Delay(delay, cancellationToken);
                     delay *= 2;
                 }
             }
@@ -42,7 +49,7 @@ namespace ADLib.Util
             [InstantHandle] Action action, string introMessage, int numRetries = 3, int delay = 3000)
 
         {
-            OnExceptionAsync(() => Task.Run(action), introMessage, numRetries, delay).Wait();
+            OnExceptionAsync(() => Task.Run(action), introMessage, CancellationToken.None, numRetries, delay).Wait();
         }
     }
 }
