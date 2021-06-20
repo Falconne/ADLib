a934f39816751094dcbf1b2e19823e0ae909ccc7
Anuradha Dissanayake <falconne@gmail.com>
2021-06-20 17:57:30 +1200

    ThrottledWebClient

---
+++ DotNet/Net/ThrottledWebClient.cs
@@ -0,0 +1,32 @@
+﻿using ADLib.Logging;
+using HtmlAgilityPack;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ADLib.Net
+{
+    public class ThrottledWebClient
+    {
+        private readonly HtmlWeb _webClient = new();
+
+        public int MinDelayMilliseconds = 20;
+
+        private DateTime _lastCallTime = DateTime.MinValue;
+
+
+        public async Task<HtmlDocument> GetPageAsync(string url, CancellationToken cancellationToken)
+        {
+            var timeSinceLastCall = (DateTime.Now - _lastCallTime).Milliseconds;
+            if (timeSinceLastCall < MinDelayMilliseconds)
+            {
+                var sleepTime = MinDelayMilliseconds - timeSinceLastCall;
+                await Task.Delay(sleepTime, cancellationToken);
+            }
+
+            GenLog.Debug($"Fetching {url}");
+            _lastCallTime = DateTime.Now;
+            return await _webClient.LoadFromWebAsync(url, cancellationToken);
+        }
+    }
+}
