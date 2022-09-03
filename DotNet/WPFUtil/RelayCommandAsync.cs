49111862c27f95b6b80a40cc54362ece284faf07
evilbert <evilbert90@gmail.com>
2022-09-03 18:43:57 +1200

    Deleting dir to recycle bin

---
+++ DotNet/WPFUtil/RelayCommandAsync.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFUtil
+{
+    public class RelayCommandAsync : ICommand
+    {
+        private readonly Func<Task> _execute;
+
+        private readonly Func<bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommandAsync(Func<Task> execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _ = _execute();
+        }
+    }
+}
