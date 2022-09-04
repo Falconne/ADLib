1c5440e8187edca02cf27df6473e8913cc19edb9
evilbert <evilbert90@gmail.com>
2022-09-04 18:02:22 +1200

    File system item helper

---
+++ DotNet/Util/FileSystemItem.cs
@@ -0,0 +1,24 @@
+﻿namespace ADLib.Util;
+
+public class FileSystemItem
+{
+    public readonly string OriginalPath;
+
+    public string Filename => Path.GetFileName(OriginalPath);
+
+    public FileSystemItem(string originalPath)
+    {
+        OriginalPath = originalPath;
+    }
+
+    public string GetContainingDirectory()
+    {
+        var dir = Path.GetDirectoryName(OriginalPath);
+        if (dir == null)
+        {
+            throw new Exception($"Unexpected root level path: {OriginalPath}");
+        }
+
+        return dir;
+    }
+}
