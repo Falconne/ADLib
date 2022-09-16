e8e6b332b62004da0dded21a307ab5b2e4764b37
evilbert <evilbert90@gmail.com>
2022-09-17 07:51:52 +1200

    foreach with index helper

--- DotNet/Util/Extensions.cs
+++ DotNet/Util/Extensions.cs
@@ -24,5 +24,9 @@ namespace ADLib.Util
             return !string.IsNullOrWhiteSpace(str);
         }
 
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
+        {
+            return source.Select((item, index) => (item, index));
+        }
     }
 }
