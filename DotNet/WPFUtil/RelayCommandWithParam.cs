60f32e7dc5f771e2d2808885d542b8b386b140c9
evilbert <evilbert90@gmail.com>
2022-10-02 20:55:14 +1300

    Relay helper

---
+++ DotNet/WPFUtil/RelayCommandWithParam.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Windows.Input;
+
+namespace WPFUtil
+{
+    public class RelayCommandWithParam : ICommand
+    {
+        private readonly Action<object?> _execute;
+
+        private readonly Func<bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public RelayCommandWithParam(Action<object?> execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
