aa87b2ebc53c4c1bde77246ee23736bcf2feac34
Anu on ppc-dev <falconne@gmail.com>
2022-12-20 20:18:18 +1300

    Generic throttled object

---
+++ DotNet/Util/ThrottledObject.cs
@@ -0,0 +1,43 @@
+﻿namespace ADLib.Util;
+
+public class ThrottledObject<T> where T : notnull
+{
+    public readonly int MinDelayMilliseconds;
+
+    private DateTime _lastCallTime = DateTime.MinValue;
+
+    private readonly T _object;
+
+    private readonly CancellationToken _cancellationToken;
+
+    public ThrottledObject(T o, int minDelayMilliseconds, CancellationToken cancellationToken)
+    {
+        MinDelayMilliseconds = minDelayMilliseconds;
+        _cancellationToken = cancellationToken;
+        _object = o ?? throw new ArgumentNullException(nameof(o));
+    }
+
+    public async Task<T> GetAsync()
+    {
+        var timeSinceLastCall = (DateTime.Now - _lastCallTime).Milliseconds;
+        if (timeSinceLastCall < MinDelayMilliseconds)
+        {
+            var sleepTime = MinDelayMilliseconds - timeSinceLastCall;
+            await Task.Delay(sleepTime, _cancellationToken);
+        }
+
+        _lastCallTime = DateTime.Now;
+        return _object;
+    }
+
+    public T Get()
+    {
+        return GetAsync().Result;
+    }
+
+    public T GetWithoutThrottle()
+    {
+        _lastCallTime = DateTime.Now;
+        return _object;
+    }
+}
