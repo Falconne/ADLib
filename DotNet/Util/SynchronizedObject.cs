20d5240f186c561dd1ffb1befa7cf00ddf8c958f
Anu on ppc-dev <falconne@gmail.com>
2022-12-25 09:13:00 +1300

    Add SynchronizedObject

---
+++ DotNet/Util/SynchronizedObject.cs
@@ -0,0 +1,30 @@
+﻿namespace ADLib.Util
+{
+    public class SynchronizedObject<T>
+    {
+        private readonly object _lock = new();
+
+        private T? _data;
+
+        public SynchronizedObject(T? data)
+        {
+            _data = data;
+        }
+
+        public T? Get()
+        {
+            lock (_lock)
+            {
+                return _data;
+            }
+        }
+
+        public void Set(T? value)
+        {
+            lock (_lock)
+            {
+                _data = value;
+            }
+        }
+    }
+}
