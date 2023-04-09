1d56239266d9b6ee6ae20275c647ffcc5ccac6a7
Anu on ppc-dev <falconne@gmail.com>
2023-04-09 15:22:26 +1200

    Exception handler helper

---
+++ DotNet/WPFUtil/TopLevelExceptionHandler.cs
@@ -0,0 +1,53 @@
+﻿using ADLib.Logging;
+using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WPFUtil;
+
+public static class TopLevelExceptionHandler
+{
+    public static Action<Exception> Attach()
+    {
+        AppDomain.CurrentDomain.UnhandledException += HandleGlobalException;
+        Application.Current.DispatcherUnhandledException += HandleMainThreadException;
+        TaskScheduler.UnobservedTaskException += HandleUnobservedTaskException;
+
+        return ShowError;
+    }
+
+    private static void HandleUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+    {
+        e.SetObserved();
+        var innerException = e.Exception.InnerException;
+        ShowError(innerException ?? e.Exception);
+    }
+
+    private static void HandleMainThreadException(object sender, DispatcherUnhandledExceptionEventArgs e)
+    {
+        ShowError(e.Exception);
+    }
+
+    private static void HandleGlobalException(object sender, UnhandledExceptionEventArgs args)
+    {
+        ShowError((Exception)args.ExceptionObject);
+    }
+
+    private static void ShowError(Exception e)
+    {
+        GenLog.Error($"Unhandled Exception: {e.Message}");
+        if (e.StackTrace != null)
+        {
+            GenLog.Error(e.StackTrace);
+        }
+
+        MessageBox.Show(
+            $"{e.Message}", "Unhandled Exception",
+            MessageBoxButton.OK,
+            MessageBoxImage.Error);
+
+        Environment.Exit(1);
+    }
+
+}
