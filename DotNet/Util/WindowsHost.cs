8c47b8664e3159e3edc3ff5c19347670c4db3431
Anu on ppc-dev <falconne@gmail.com>
2023-04-10 20:55:04 +1200

    URL opening helper

--- DotNet/Util/WindowsHost.cs
+++ DotNet/Util/WindowsHost.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using ADLib.Logging;
+﻿using ADLib.Logging;
+using System.Diagnostics;
 
-namespace ADLib.Util
+namespace ADLib.Util;
+
+public static class WindowsHost
 {
-    public static class WindowsHost
+    public static void Restart(int delay)
     {
-        public static void Restart(int delay)
-        {
-            GenLog.Info($"Restarting computer in {delay} seconds");
-            Shell.RunAndFailIfNotExitZero($@"{Environment.SystemDirectory}\shutdown.exe", "/r", "/t", delay);
-        }
+        GenLog.Info($"Restarting computer in {delay} seconds");
+        Shell.RunAndFailIfNotExitZero($@"{Environment.SystemDirectory}\shutdown.exe", "/r", "/t", delay);
+    }
+
+    public static void OpenInDefaultBrowser(string url)
+    {
+        GenLog.Info($"Opening in browser: {url}");
+        Process.Start(new ProcessStartInfo { FileName = url, UseShellExecute = true });
     }
 }
