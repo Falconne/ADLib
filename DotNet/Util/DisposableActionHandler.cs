367081b23b1b58852e9349c5500e27b44401b2da
Anu on ppc-dev <falconne@gmail.com>
2023-05-18 21:31:40 +1200

    Generic DisposableActionHandler

---
+++ DotNet/Util/DisposableActionHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace ADLib.Util;
+
+public class DisposableActionHandler : IDisposable
+{
+    public DisposableActionHandler(Action disposeAction)
+    {
+        _disposeAction = disposeAction;
+    }
+
+    private readonly Action _disposeAction;
+
+    public void Dispose()
+    {
+        _disposeAction();
+    }
+
+    public static DisposableActionHandler CreateWithInit(
+        Action initAction,
+        Action disposeAction)
+    {
+        initAction();
+        return new DisposableActionHandler(disposeAction);
+    }
+}
---
+++ DotNet/Util/DisposableActionHandlerAsync.cs
@@ -0,0 +1,24 @@
+﻿namespace ADLib.Util;
+
+public class DisposableActionHandlerAsync : IAsyncDisposable
+{
+    public DisposableActionHandlerAsync(Func<Task> disposeAction)
+    {
+        _disposeAction = disposeAction;
+    }
+
+    private readonly Func<Task> _disposeAction;
+
+    public async ValueTask DisposeAsync()
+    {
+        await _disposeAction();
+    }
+
+    public static async Task<DisposableActionHandlerAsync> CreateWithInit(
+        Func<Task> initAction,
+        Func<Task> disposeAction)
+    {
+        await initAction();
+        return new DisposableActionHandlerAsync(disposeAction);
+    }
+}
