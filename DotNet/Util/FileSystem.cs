a9102a73051bf01c12577bba8361007602865dd7
Anu on ppc-dev <falconne@gmail.com>
2023-08-06 13:00:05 +1200

    Formatting

--- DotNet/Util/FileSystem.cs
+++ DotNet/Util/FileSystem.cs
@@ -11,7 +11,9 @@ public static class FileSystem
     public static void CreateDirectory(string path)
     {
         if (Directory.Exists(path))
+        {
             return;
+        }
 
         GenLog.Info($"Creating directory {path}");
         Directory.CreateDirectory(path);
@@ -61,7 +63,9 @@ public static class FileSystem
         }
 
         if (!File.Exists(path))
+        {
             return;
+        }
 
         Retry.OnException(() => File.Delete(path), $"Deleting {path}");
     }
@@ -75,7 +79,9 @@ public static class FileSystem
         }
 
         if (!File.Exists(path))
+        {
             return;
+        }
 
         await Retry.OnExceptionAsync(
             async () => await Task.Run(() => File.Delete(path)),
@@ -198,14 +204,18 @@ public static class FileSystem
     public static string MoveFileToDir(string file, string dir, bool makeUnique = false)
     {
         if (!File.Exists(file))
+        {
             throw new InvalidOperationException($"Cannot move non-existent file: {file}");
+        }
 
         CreateDirectory(dir);
         var fileName = Path.GetFileName(file);
         var dest = makeUnique ? GetUniquelyNamedFileIn(dir, fileName) : Path.Combine(dir, fileName);
 
         if (File.Exists(dest))
+        {
             throw new InvalidAssumptionException($"Cannot move '{file}' to '{dest}'. Target already exists.");
+        }
 
         Retry.OnException(() => File.Move(file, dest), $"Moving '{file}' to dir '{dir}'");
         return dest;
@@ -241,13 +251,19 @@ public static class FileSystem
     public static async Task DeleteFileToRecycleBinAsync(string? path, CancellationToken cancellationToken)
     {
         if (path!.IsEmpty())
+        {
             return;
+        }
 
         if (Directory.Exists(path))
+        {
             throw new InvalidAssumptionException($"Asked to delete file, but is a directory: {path}");
+        }
 
         if (!File.Exists(path))
+        {
             return;
+        }
 
         await Retry.OnExceptionAsync(
             async () => await Task.Run(
@@ -268,13 +284,19 @@ public static class FileSystem
     public static async Task DeleteDirToRecycleBinAsync(string? path, CancellationToken cancellationToken)
     {
         if (path.IsEmpty())
+        {
             return;
+        }
 
         if (File.Exists(path))
+        {
             throw new InvalidAssumptionException($"Asked to delete directory, but is a file: {path}");
+        }
 
         if (!Directory.Exists(path))
+        {
             return;
+        }
 
         await Retry.OnExceptionAsync(
             async () => await Task.Run(
@@ -291,16 +313,22 @@ public static class FileSystem
     public static async Task<string[]> GetFilesUnderFastAsync(string dir, bool recurse = false)
     {
         if (!Directory.Exists(dir))
+        {
             throw new InvalidAssumptionException($"Directory not found: {dir}");
+        }
 
         var parameters = new List<object> { "/c", "dir", "/b", dir };
         if (recurse)
+        {
             parameters.Add("/s");
+        }
 
         var allFilesRaw = await Shell.RunSilentAndFailIfNotExitZeroAsync("cmd.exe", parameters.ToArray());
 
         if (allFilesRaw.IsEmpty())
+        {
             return Array.Empty<string>();
+        }
 
         return allFilesRaw.Split(Environment.NewLine)
             .Where(f => f.IsNotEmpty())
@@ -324,7 +352,9 @@ public static class FileSystem
         CancellationToken stoppingToken)
     {
         if (!Directory.Exists(root))
+        {
             return Array.Empty<string>();
+        }
 
         GenLog.Debug($"Finding all files under {root}");
         return await Task.Run(
@@ -338,7 +368,9 @@ public static class FileSystem
     public static string GetUniquelyNamedFileIn(string dir, string baseFilename)
     {
         if (!Directory.Exists(dir))
+        {
             throw new InvalidOperationException($"Directory does not exist: {dir}");
+        }
 
         var basename = Path.GetFileNameWithoutExtension(baseFilename);
         var extension = Path.GetExtension(baseFilename);
@@ -349,7 +381,9 @@ public static class FileSystem
             var remoteFilename = $"{basename}{suffix}{extension}";
             var remotePath = Path.Combine(dir, remoteFilename);
             if (!File.Exists(remotePath))
+            {
                 return remotePath;
+            }
 
             index++;
         }
@@ -358,7 +392,9 @@ public static class FileSystem
     private static void DeleteDirectory(string path)
     {
         if (string.IsNullOrWhiteSpace(path))
+        {
             return;
+        }
 
         var retries = 10;
         while (Directory.Exists(path))
