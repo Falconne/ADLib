465757b45290f4e1e491a2c1fdf92c752b562c29
Anu on ppc-dev <falconne@gmail.com>
2025-04-23 21:04:54 +1200

    Helper for relay commands

---
+++ DotNet/WPFUtil/RelayCommandFactory.cs
@@ -0,0 +1,29 @@
+﻿using ADLib.Util;
+using System;
+using System.Threading.Tasks;
+
+namespace WPFUtil;
+
+public class RelayCommandFactory
+{
+    public Func<bool>? CanExecute { get; set; }
+
+    public ExceptionHandler ErrorHandler { get; set; } = TopLevelExceptionHandler.ShowError;
+
+    public SafeSynchronizedObject<bool>? SharedBusyIndicator { get; set; }
+
+    public RelayCommandAsync Create(Func<Task> action)
+    {
+        var command = new RelayCommandAsync(
+            action,
+            ErrorHandler,
+            CanExecute);
+
+        if (SharedBusyIndicator != null)
+        {
+            command.WithSharedBusyIndicator(SharedBusyIndicator);
+        }
+
+        return command;
+    }
+}
