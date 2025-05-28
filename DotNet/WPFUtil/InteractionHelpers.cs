8f678da87feef188ab68d7b007379d2f433b8e07
Anu on ppc-dev <falconne@gmail.com>
2025-05-28 22:36:59 +1200

    InteractionHelpers confirmation box

---
+++ DotNet/WPFUtil/InteractionHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace WPFUtil;
+
+public static class InteractionHelpers
+{
+    public static bool GetConfirmation(string message, string title)
+    {
+        return Application.Current.Dispatcher.Invoke(
+            () => MessageBox.Show(
+                      Application.Current.MainWindow!,
+                      message,
+                      title,
+                      MessageBoxButton.YesNo,
+                      MessageBoxImage.Question)
+                  == MessageBoxResult.Yes);
+    }
+}
