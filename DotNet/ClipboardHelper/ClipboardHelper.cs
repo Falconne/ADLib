8041a73eef28555b0c1880ce726fbecbd5fe0640
Anu on ppc-dev <falconne@gmail.com>
2025-07-06 13:28:35 +1200

    Don't hang if ClipboardHelper keeps failing

--- DotNet/ClipboardHelper/ClipboardHelper.cs
+++ DotNet/ClipboardHelper/ClipboardHelper.cs
@@ -66,44 +66,64 @@ public static class ClipboardHelper
         }
     }
 
-    public static async Task<(string? sourceUrl, string? clipText, string? rawHtml)> GetCurrentTextContent(
+    public static Task<(string? sourceUrl, string? clipText, string? rawHtml)> GetCurrentTextContent(
         CancellationToken cancellationToken = default)
-
     {
-        while (true)
+        try
         {
-            try
-            {
-                return await GetCurrentTextContentUnsafe(cancellationToken);
-            }
-            catch (Exception e)
-            {
-                GenLog.Debug($"Ignoring clipboard error: {e.Message}");
-                await Task.Delay(100, cancellationToken);
-            }
+            return RetryClipboardOperationAsync(GetCurrentTextContentUnsafe, cancellationToken);
+        }
+        catch (Exception)
+        {
+            return Task
+                .FromResult<(string?, string?, string?)>((null, null, null));
         }
     }
 
-    public static async Task<BitmapSource?> GetCurrentImageContent(
+    public static Task<BitmapSource?> GetCurrentImageContent(
         CancellationToken cancellationToken = default)
     {
-        while (true)
+        try
         {
-            try
-            {
-                return GetClipboardImageUnsafe();
-            }
-            catch (Exception e)
-            {
-                GenLog.Debug($"Ignoring clipboard image error: {e.Message}");
-                await Task.Delay(100, cancellationToken);
-            }
+            return RetryClipboardOperationAsync(
+                _ => Task.FromResult(GetClipboardImageUnsafe()),
+                cancellationToken);
+        }
+        catch (Exception)
+        {
+            return Task.FromResult<BitmapSource?>(null);
         }
     }
 
     private static BitmapSource? GetClipboardImageUnsafe()
     {
         return Clipboard.ContainsImage() ? Clipboard.GetImage() : null;
+    }
+
+    private static async Task<T> RetryClipboardOperationAsync<T>(
+        Func<CancellationToken, Task<T>> operation,
+        CancellationToken cancellationToken = default)
+    {
+        var tries = 0;
+        while (++tries < 10)
+        {
+            if (tries > 1)
+            {
+                GenLog.Debug($"Retrying clipboard operation, attempt {tries}...");
+            }
+
+            try
+            {
+                return await operation(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                GenLog.Debug($"Ignoring clipboard error: {e.Message}");
+                await Task.Delay(200, cancellationToken);
+            }
+        }
+
+        throw new Exception("Failed to get clipboard content after multiple attempts.");
     }
 
     private static async Task<(string? sourceUrl, string? clipText, string? rawHtml)>
