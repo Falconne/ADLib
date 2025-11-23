bf53d885279f1f336c64b3581ff8551c4f501f2b
Anu on ppc-dev <falconne@gmail.com>
2025-11-23 14:58:11 +1300

    Dispatcher helper

---
+++ DotNet/WPFUtil/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFUtil;
+
+public static class WPFHelpers
+{
+    public static async Task InvokeInUIThreadAsync(System.Func<Task> action)
+    {
+        await Application.Current.Dispatcher.InvokeAsync(async () => await action().ConfigureAwait(false));
+    }
+
+    public static void InvokeInUIThread(Action action)
+    {
+        Application.Current.Dispatcher.Invoke(action);
+    }
+}
